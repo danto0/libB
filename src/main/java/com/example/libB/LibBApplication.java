cde7aa9a302cf37f7ae0147f086dc2bf367356e3
dantolino <david.antolinorivas@igt.com>
2023-05-15 16:51:00 +0200

    init

---
+++ src/main/java/com/example/libB/LibBApplication.java
@@ -0,0 +1,13 @@
+package com.example.libB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/libB/LibBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
