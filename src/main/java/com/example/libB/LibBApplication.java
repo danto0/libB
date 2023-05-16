c57ae1df77d5fee0537b67d549650e05531218b6
dantolino <david.antolinorivas@igt.com>
2023-05-16 12:45:24 +0200

    add dependencies

--- src/main/java/com/example/libB/LibBApplication.java
+++ src/main/java/com/example/libB/LibBApplication.java
@@ -1,13 +1,20 @@
 package com.example.libB;
 
-import org.springframework.boot.SpringApplication;
+import com.example.lib.WRCar;
+//import com.google.common.base.*;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LibBApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LibBApplication.class, args);
+//		SpringApplication.run(LibBApplication.class, args);
+		WRCar car = new WRCar(45);
+//		System.out.println(Strings.repeat("hi", 3));
+//		Strings.repeat("hi", 4);
+
+
 	}
 
 }
